c37814cb931aca0d418a680614543e42c7a325fa
Paul <Paul@192.168.1.5>
2016-03-30 07:59:52 +1030

    Update

--- src/finaldigit/FinalDigit.java
+++ src/finaldigit/FinalDigit.java
@@ -9,6 +9,8 @@ import bgi.SimpleDigit;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -29,15 +31,40 @@ public class FinalDigit {
     
      private static void demoGUI() {
         JFrame f = new JFrame("Swing Paint Demo");
+        GridBagLayout experimentLayout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(250,250);
-        f.setVisible(true);
         f.setResizable(true);
-        ClockDigit test = new ClockDigit();
+        f.setLayout(experimentLayout);
+        ClockDigit t1 = new ClockDigit();
+        ClockDigit t2 = new ClockDigit();
+        ClockDigit t3 = new ClockDigit();
+        ClockDigit t4 = new ClockDigit();
+        ClockDigit t5 = new ClockDigit();
 //        SimpleDigit s = new SimpleDigit(7,);
 //        s.setDigit(8);
-        test.setDigit(3);
-        f.add(test);
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+        t1.setDigit(1);
+        t2.setDigit(2);
+        t4.setDigit(0);
+        t5.setDigit(0);
+        String[] text = {"ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN"};
+        
+        t2.setText(text);  
+        t2.setTextAlignment(t2.RIGHT);
+        t3.setText(text);
+        t3.setTextAlignment(t3.CENTER);
+        t4.setText(text);
+        t4.setTextAlignment(t4.LEFT);
+        f.add(t1,c);
+        f.add(t2,c);
+        f.add(t3,c);
+        f.add(t4,c);
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        f.add(t5,c);
         f.pack();
+        f.setVisible(true);
     }
 }
